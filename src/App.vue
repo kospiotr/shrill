30ddcb043149d4cbff2aec6fe0e1d445f89962d1
Piotr Kosmowski <kospiotr@gmail.com>
2025-04-10 12:44:51 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ const getName = async () => {
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <p>Changed</p>
       </nav>
     </div>
   </header>
